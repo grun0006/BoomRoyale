68f835adc1e6f7209571ee7fda612dff81413f53
githubdesktopisbeter <githubdesktopisbeter@gmail.com>
2025-10-27 12:49:45 +0100

    test2

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+export default {
+  kit: {
+    adapter: adapter(),
+    csrf: false,
+    csp: {
+      mode: 'auto',
+      directives: {
+        'default-src': ['self'],
+        'img-src': ['self', 'data:', 'https:'],
+        'script-src': ['self'],
+        'style-src': ['self', 'unsafe-inline']
+      }
+    }
+  }
 };
-
-export default config;
